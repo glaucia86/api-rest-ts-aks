d25d452ebfe81bb018f24defa07cdeb715c096db
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-09-11 21:31:17 -0300

    feat: create a simple route to test the application and express config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hi, friends!')
+/**
+ * file: src/index.ts
+ * description: file responsible for the main entry point of the application.
+ * data: 09/11/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Application running on port: ${port}!`));
+
+app.get('/', (req, res) => {
+  res.send('Hi, friends!');
+});
