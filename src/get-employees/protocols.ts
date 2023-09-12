d31de73640d718a0f20a25ce578ef380a0eccb29
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-09-11 22:38:07 -0300

    feat: create a model for 'Employee'

--- src/get-employees/protocols.ts
+++ src/get-employees/protocols.ts
@@ -5,6 +5,8 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
+import { Employee } from "../models/employee";
+
 export interface IGetEmployeesController {
   handle(): any;
 }
---
+++ src/models/employee.ts
@@ -0,0 +1,14 @@
+/**
+ * file: src/models/employee.ts
+ * description: file responsible for define model of employee
+ * data: 09/11/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export interface Employee {
+  firstName: string;
+  lastName: string;
+  email: string;
+  rolePosition: string;
+  password: string;
+}
