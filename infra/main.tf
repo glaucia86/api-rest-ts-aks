97d78333231d8ec02d32d489f09cadce96e3052f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-09-22 22:10:18 -0300

    chore: create aks resource using Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -15,3 +15,21 @@ resource "azurerm_resource_group" "api_ts_aks_rg" {
   name     = "devsquad-api-aks"
   location = "West US 2"
 }
+
+resource "azurerm_kubernetes_cluster" "api_ts_aks_resource" {
+  name                = "api-ts-aks"
+  location            = azurerm_resource_group.api_ts_aks_rg.location
+  resource_group_name = azurerm_resource_group.api_ts_aks_rg.name
+  dns_prefix          = "api-aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2"
+    zones      = [1, 2, 3]
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
